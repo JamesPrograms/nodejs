4a0c577238e32b720c411e88fd2c4147f19fe7c0
james <18302819568@163.com>
2017-09-16 21:42:01 +0800

    finish the learning of  stream module.

--- stream.js
+++ stream.js
@@ -8,9 +8,9 @@ rs.on('data', function (chunk) {
     console.log(`data:${chunk}`);
 });
 // 响应流的事件：end事件，表示流已经到末尾
-rs.on('end', function () {
-    console.log('END');
-});
+//rs.on('end', function () {
+//    console.log('END');
+//});
 // 响应流的事件：error事件，表示出错
 rs.on('error', function (err) {
     console.log('ERROR: ' + err);
@@ -21,4 +21,10 @@ var ws1 = fs.createWriteStream('files/output1.txt', 'utf-8');
 ws1.write('使用Stream写入文本数据...\n');
 ws1.write('END.');
 ws1.end();
+
+/*将readableStream.txt的数据通过pipe流入writableStream.txt*/
+var rs = fs.createReadStream('files/readableStream.txt');
+var ws = fs.createWriteStream('files/writableStream.txt');
+rs.pipe(ws);
+
 
